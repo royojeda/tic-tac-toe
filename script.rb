7952cd61282cc2e2a331688ad633923d40fdd656
Dennis Ojeda <royojeda1@gmail.com>
2022-05-04 20:22:50 +0800

    Add logic to check rows for game end conditions

--- script.rb
+++ script.rb
@@ -19,7 +19,8 @@ class Move
   end
 
   def self.finished?
-    check_column_one || check_column_two || check_column_three
+    check_column_one || check_column_two || check_column_three ||
+    check_row_one || check_row_two || check_row_three
   end
 
   def self.new_turn
@@ -53,6 +54,36 @@ class Move
     if @@board[2] != ' ' &&
        @@board[5] == @@board[2] &&
        @@board[8] == @@board[5]
+      return true
+    end
+
+    false
+  end
+
+  def self.check_row_one
+    if @@board[0] != ' ' &&
+       @@board[1] == @@board[0] &&
+       @@board[2] == @@board[1]
+      return true
+    end
+
+    false
+  end
+
+  def self.check_row_two
+    if @@board[3] != ' ' &&
+       @@board[4] == @@board[3] &&
+       @@board[5] == @@board[4]
+      return true
+    end
+
+    false
+  end
+
+  def self.check_row_three
+    if @@board[6] != ' ' &&
+       @@board[7] == @@board[6] &&
+       @@board[8] == @@board[7]
       return true
     end
 
