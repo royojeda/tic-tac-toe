c6554311a7f8b3dc73ce0d1238fb13b5b70ad07d
Dennis Ojeda <royojeda1@gmail.com>
2022-05-04 18:10:11 +0800

    Add logic to get user input and update board

--- script.rb
+++ script.rb
@@ -1,14 +1,15 @@
 class Move
-  @@board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  @@board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
   def initialize(player, move)
     @player = player
-    @move = move
+    @move = move.to_i
     make_move
     show_board
   end
 
   def show_board
+    system 'clear'
     puts " #{@@board[0]} | #{@@board[1]} | #{@@board[2]} "
     puts '---|---|---'
     puts " #{@@board[3]} | #{@@board[4]} | #{@@board[5]} "
@@ -16,15 +17,23 @@ class Move
     puts " #{@@board[6]} | #{@@board[7]} | #{@@board[8]} "
   end
 
+  def self.finished?
+    false
+  end
+
   private
 
-  attr_accessor :player, :move
+  attr_reader :player, :move
 
   def make_move
-    @@board[self.move - 1] = player
+    @@board[move - 1] = player
   end
 end
 
-turn1 = Move.new("X", 5)
-puts "---------------------------------"
-turn2 = Move.new("O", 3)
+current_player = 'X'
+until Move.finished?
+  puts "Player #{current_player}'s turn. Please enter your move: "
+  move = gets.chomp
+  Move.new(current_player, move)
+  current_player = current_player == 'X' ? 'O' : 'X'
+end
