795cb36280f80ab419d8584b1271205725f769c4
Dennis Ojeda <royojeda1@gmail.com>
2022-05-04 15:49:29 +0800

    Add logic for recording moves and updating board

--- script.rb
+++ script.rb
@@ -1,9 +1,30 @@
-def display_grid
-  puts " 1 | 2 | 3 "
-  puts "---|---|---"
-  puts " 4 | 5 | 6 "
-  puts "---|---|---"
-  puts " 7 | 8 | 9 "
+class Move
+  @@board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+  def initialize(player, move)
+    @player = player
+    @move = move
+    make_move
+    show_board
+  end
+
+  def show_board
+    puts " #{@@board[0]} | #{@@board[1]} | #{@@board[2]} "
+    puts '---|---|---'
+    puts " #{@@board[3]} | #{@@board[4]} | #{@@board[5]} "
+    puts '---|---|---'
+    puts " #{@@board[6]} | #{@@board[7]} | #{@@board[8]} "
+  end
+
+  private
+
+  attr_accessor :player, :move
+
+  def make_move
+    @@board[self.move - 1] = player
+  end
 end
 
-display_grid()
+turn1 = Move.new("X", 5)
+puts "---------------------------------"
+turn2 = Move.new("O", 3)
