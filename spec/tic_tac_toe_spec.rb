cbfbbaca0386cfaf495b06972b4eeae12e45e324
Dennis Ojeda <royojeda1@gmail.com>
2022-05-23 04:17:08 +0800

    Add a test for Player#make_move
    In the context of a valid input

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,6 +2,16 @@ require './lib/player'
 
 describe Player do
   describe '#make_move' do
+    subject(:player_move) { described_class.new('X') }
+    context 'when given a valid input' do
+      before do
+        valid_input = '7'
+        allow(player_move).to receive(:gets).and_return(valid_input)
+      end
 
+      it 'writes thte integer value of the input to @move' do
+        expect { player_move.make_move }.to change { player_move.instance_variable_get(:@move) }.to(7)
+      end
+    end
   end
 end
